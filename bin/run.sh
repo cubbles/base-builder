01a1787fba18132da98a62c56b6211f10bf60486
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2015-03-03 15:57:21 +0100

    PLAT-25: extended run-Script by the ability to push images

--- bin/run.sh
+++ bin/run.sh
@@ -16,13 +16,16 @@ showEnvironment
 echo
 echo "Do processing ..."
 echo "================="
-# check for a passed image tag
-IMAGE_TAG="latest"
-if [ ! -z "$3" ]; then { IMAGE_TAG=$3; } fi
+
 # etc contains the decking.json
 cd etc
 case "$1" in
 	build)
+		# check for a passed image tag
+		IMAGE_TAG="latest"
+		if [ ! -z "$3" ]; then { IMAGE_TAG=$3; }
+		fi
+		# start building
 		echo "building image $2:$IMAGE_TAG" 
 #		ERROUT=$(node $NODE_MODULE_DECKING/bin/decking build >$2)
 		node $NODE_MODULE_DECKING/bin/decking build $2 --tag $IMAGE_TAG
@@ -34,15 +37,30 @@ case "$1" in
 #		esac
 		;;
 
-	status)
-		echo "check the status of a cluster's containers"
-		node $NODE_MODULE_DECKING/bin/decking status $2
+	push)
+		# check for a passed container name
+		if [ ! -z "$3" ]; then {
+			echo "pushing image $3"
+			docker login -u docker -p $2 -e hrbu@incowia.com docker.webblebase.net
+			docker push $3
+			exit 0
+		}
+		else {
+			echo "usage: push [password] [image]"
+			exit 1
+		}
+		fi
+		;;
+
+	clean)
+		echo "remove untagged images"
+		docker rmi $(docker images -q --filter "dangling=true")
 		exit 0
 		;;
 
 
 	*)
-		echo "usage: $0 { build [image | all] | status [cluster] }" >&2
+		echo "usage: $0 { build [image | all] | push [password] [image] | clean }" >&2
 		exit 1
 		;;
 esac
