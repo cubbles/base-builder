fe8220ec4abced93e90f6d6dd0dcb654e0eb458c
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2015-03-13 12:22:46 +0100

    PLAT-25: fixed ssl-config for base.gateway

--- README.md
+++ README.md
@@ -5,36 +5,10 @@ Zweck: Tool für das Bauen von Docker-Images
 
 ### List Options
     $ ./bin/run.sh
-    Prepare environment ...
-    ======================
-    PATH=/mnt/sda1/projects.webbles/git/base/base-builder/opt/node_i686/bin:/home/docker/.local/bin:/usr/local/sbin:/usr/local/bin:/apps/bin:/usr/sbin:/usr/bin:/sbin:/bin
-    node version = v0.10.35
 
-    Do processing ...
-    =================
-    usage: ./bin/run.sh { build [image | all] | status [cluster] }
-    docker@boot2docker:/mnt/sda1/projects.webbles/git/base/base-builder$
-
-
-### Build and (optionally) tag images
-    # $ ./bin/run.sh build <image name | all> <optional: tag>
-    $ ./bin/run.sh build docker.webblebase.net/base/gateway 1.0
-
-    Prepare environment ...
-    ======================
-    PATH=/mnt/sda1/projects.webbles/git/base/base-builder/opt/node_i686/bin:/home/docker/.local/bin:/usr/local/sbin:/usr/local/bin:/apps/bin:/usr/sbin:/usr/bin:/sbin:/bin
-    node version = v0.10.35
-
-    Do processing ...
-    =================
-    building image docker.webblebase.net/base/gateway:1.0-SNAPSHOT
-    Using /mnt/sda1/projects.webbles/git/base/base-builder/etc/gateway as build context
-    ADD and COPY directives must be relative to the above directory
-
-    Building image docker.webblebase.net/base/gateway:1.0-SNAPSHOT
-    Uploading compressed context...
-    Step 0 : FROM nginx:1.7.9
-     ---> 4b5657a3d162
+### Build and tag images
+    # $ ./bin/run.sh build <image name | all> <tag>
+    $ $ ./bin/run.sh build docker.webblebase:4444.net/base/gateway 1.0
     [...]
 
 
--- bin/run.sh
+++ bin/run.sh
@@ -5,51 +5,55 @@
 # make sure, the user has done a cd into the bundle (otherwise relative paths do not work)
 if [ ! -e bin/check-file ]; then { echo >&2 "Please cd into the bundle before running this script."; exit 1; }
 fi
-
-echo 
-echo "Prepare environment ..."
-echo "======================"
+echo
+echo "executing: " $PWD $0 $@
+echo
+# "Prepare environment ..."
 . bin/setenv-default.sh
 . bin/setenv-local.sh
 showEnvironment
 
-echo
-echo "Do processing ..."
-echo "================="
 
-# etc contains the decking.json
-cd etc
+# "Do processing ..."
+cd etc # contains decking.json
 case "$1" in
 	build)
-		# check for a passed image tag
-		IMAGE_TAG="latest"
-		if [ ! -z "$3" ]; then { IMAGE_TAG=$3; }
+		if [ ! $# -eq 3 ]; then {
+			echo "Purpose: Build images"
+			echo "Usage: build [image | all] [tag]"
+			exit 1
+		}
+		else {
+			IMAGE_TAG="latest"
+			if [ ! -z "$3" ]; then { IMAGE_TAG=$3; }
+			fi
+			# start building
+			echo "building image $2:$IMAGE_TAG"
+			node $NODE_MODULE_DECKING/bin/decking build $2 --tag $IMAGE_TAG
+		}
 		fi
-		# start building
-		echo "building image $2:$IMAGE_TAG" 
-		node $NODE_MODULE_DECKING/bin/decking build $2 --tag $IMAGE_TAG
 		;;
 
 	push)
-		# check for a passed image name
-		if [ ! -z "$3" ]; then {
-			echo "pushing image $3"
-			docker login -u docker -p $2 -e hrbu@incowia.com docker.webblebase.net
+		if [ ! $# -eq 4 ]; then {
+			echo "Purpose: Push images into the project-docker-registry"
+			echo "Usage: push [password] [image | all] [tag]"
+			exit 1
+		}
+		else {
+			echo "pushing image $3 with tag $4"
+			docker login -u docker -p $2 -e hrbu@incowia.com docker.webblebase.net:444
 			if [ "$3" == "all" ]; then {
-				docker push "docker.webblebase.net/base/gateway":$4
-				docker push "docker.webblebase.net/base/coredatastore":$4
-				docker push "docker.webblebase.net/base/coredatastore_volume":$4
-				docker push "docker.webblebase.net/base/serviceconfigvolume":$4
+				docker push "docker.webblebase.net:444/base/gateway":$4
+				docker push "docker.webblebase.net:444/base/coredatastore":$4
+				docker push "docker.webblebase.net:444/base/coredatastore_volume":$4
+				docker push "docker.webblebase.net:444/base/serviceconfigvolume":$4
 			}
 			else {
 				docker push $3:$4
 			}
 			fi
 			exit 0
-		}
-		else {
-			echo "usage: push [password] [image | all] [tag]"
-			exit 1
 		}
 		fi
 		;;
@@ -62,7 +66,7 @@ case "$1" in
 
 
 	*)
-		echo "usage: $0 { build [image | all] | push [password] [image | all] [tag] | clean }" >&2
+		echo "usage: $0 { build | push | clean }" >&2
 		exit 1
 		;;
 esac
