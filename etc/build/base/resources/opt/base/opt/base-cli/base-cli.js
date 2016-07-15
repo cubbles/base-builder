1ac82092d1e69901feea490a647c8b93a567ca0e
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2016-07-15 14:13:10 +0200

    Added a 'pull' command to the base-cli to allow admins to prepare a base-upgrade before going offline with the existing version.

--- etc/build/base/resources/opt/base/opt/base-cli/base-cli.js
+++ etc/build/base/resources/opt/base/opt/base-cli/base-cli.js
@@ -6,6 +6,7 @@ var path = require('path');
 var vorpal = require('vorpal')();
 
 //load commands
+require('./lib/setup/pull.js')(vorpal);
 require('./lib/runtime-controller/up.js')(vorpal);
 require('./lib/runtime-controller/down.js')(vorpal);
 require('./lib/runtime-controller/restart.js')(vorpal);
